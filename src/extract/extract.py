f77477e138a4de9b9986b246c4fb026f109791bd
Carles Vila Gelada <litusvila199@gmail.com>
2026-09-08 20:07:42 +0200

    Add S3 raw data storage

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,18 +1,24 @@
 import requests
+import boto3
+import json
 
+from datetime import datetime, timezone
 from src.utils.logger import get_logger
-from src.config.settings import API_URL, CRYPTO_IDS
+from src.config.settings import API_URL, CRYPTO_IDS, S3_BUCKET
 
 logger = get_logger(__name__)
 
+s3 = boto3.client("s3")
+
 def extract_data():
     try:
-        response = response = requests.get(
+        response = requests.get(
             API_URL,
             params={
                 "ids": ",".join(CRYPTO_IDS),
                 "vs_currencies": "usd"
-            }
+            },
+            timeout=10
         )
         response.raise_for_status()
 
@@ -26,6 +32,27 @@ def extract_data():
         logger.error(f"Error extracting data: {e}")
         raise
 
+def save_to_s3(data):
+    json_data = json.dumps(data)
+
+    timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%d/%H-%M-%S")
+
+    try:
+        s3.put_object(
+            Bucket=S3_BUCKET,
+            Key=f"coingecko/raw/{timestamp}/prices.json",
+            Body=json_data,
+            ContentType="application/json"
+        )
+
+        logger.info("Data saved to S3 successfully")
+
+    except Exception as e:
+        logger.error(f"Error saving data to S3: {e}")
+        raise    
+
+
+
 if __name__ == "__main__":
     data = extract_data()
-    print(data)
+    save_to_s3(data)
